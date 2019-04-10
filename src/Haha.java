7fb769769e7d16e55390d69368a1b68e011ecf9f
hkimar <hkimar@csl4wk21.win2k.cs.ust.hk>
2019-04-10 14:41:17 +0800

    trump local change

--- src/Haha.java
+++ src/Haha.java
@@ -3,6 +3,7 @@ public class Haha {
 	public static void main(String args[]) {
 		System.out.println("Haha");
 		//Add a comment
+		//Add another comment
 		System.out.println("let's meet in Hanoi.");
 	}
 }	
