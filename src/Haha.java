f731d5cf5db5d3b18f146dfecc2c2ea4e07fc78c
hkimar <hkimar@csl4wk21.win2k.cs.ust.hk>
2019-04-10 14:44:52 +0800

    test

--- src/Haha.java
+++ src/Haha.java
@@ -3,7 +3,7 @@ public class Haha {
 	public static void main(String args[]) {
 		System.out.println("Haha");
 		//Add a comment
-		System.out.println("let's meet in Hanoi.");
+		System.out.println("let's meet in Hanoi, Vietnam.");
 		System.out.println("I'm here.");
 	}
 }	
